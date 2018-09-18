0580af78e9685e5f71d9af137284a5ae255abe68
Junior331 <qualquercoisa333133127@gmail.com>
2018-09-18 11:58:08 -0300

    aula 5

--- js/app.js
+++ js/app.js
@@ -1,24 +1,39 @@
+var imageList = ["cofap-min", "logo-nakata-min", "logoa-min", "fabrini-min", "Sem-Título--min", "axios-min", "monroe-min", "metalleve-min", "skf-min", "nkg-min", "tecfil-min", "shell-min", "gates-min", "cral-min", "moura-min", "visconde-min", "schadek-min", "boch-min", "trw-min", "havoline-min", "lubrax-min", "petronas-min", "ipiranga-min", "fte-min", "valeo-min", "sachs-min", "luk-min", "siemens-min", "continental-min", "nytron-min", "ima-min", "bfx-min", "cartego-min", "taranto-min", "sabo-min", "ate-min", "controil-min", "frasle-min", "hieprfreios-min", "sampel-min", "Brosol-min", "indisa-min", "magneti-min"];
+
+var imageListHtml = gerarImageListHtml(imageList);
+
+document.querySelector("#carousel-img").innerHTML = imageListHtml;
+
+function gerarImageListHtml(lista) {
+    var listaHtml = "";
+    for (i = 0; i < lista.length; i++) {
+        listaHtml += "<img src='./img/imgm/" + lista[i] + ".png' alt=''>";
+    }
+    return listaHtml;
+}
+
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    items:9,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:5,
-            nav:false,
+    items: 9,
+    lazyLoad: true,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 5,
+            nav: false,
         },
-        600:{
-            items:7,
-            nav:false,
+        600: {
+            items: 7,
+            nav: false,
         },
-        1000:{
-            items:9,
-            nav:false,
-            loop:true,
+        1000: {
+            items: 9,
+            nav: false,
+            loop: true,
         }
     }
 });
